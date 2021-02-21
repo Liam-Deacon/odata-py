a1b711683e108673f41d0379d5e1ca5fea42aad3
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2021-02-21 12:10:30 +0000

    Add docstrings to render

--- odata/render.py
+++ odata/render.py
@@ -1,3 +1,4 @@
+"""Render payload based on Content-Type header"""
 from json import JSONEncoder, dumps
 from odata.exc import RequestParseError
 
@@ -10,11 +11,13 @@ class ReprEncoder(JSONEncoder):
         return repr(obj)
 
 
-def jsonify(payload):
+def jsonify(payload) -> str:
+    """Encode `payload` as JSON string"""
     return dumps(payload, cls=ReprEncoder)
 
 
-def plaintext(payload):
+def plaintext(payload) -> str:
+    """Encode `payload` as plaintext"""
     if hasattr(payload, 'values'):
         payload = list(payload.values())
     if isinstance(payload, list) or isinstance(payload, tuple):
@@ -24,18 +27,31 @@ def plaintext(payload):
     return payload
 
 
-#TODO: 'application/xml', 'application/xml+atom'
+def xml(payload) -> str:
+    """Encode payload to XML string"""
+    raise NotImplementedError  # TODO: implement XML translation
+
+
+def atom_xml(payload) -> str:
+    raise NotImplementedError  # TODO: implement XML translation
+
+
 formatters = {
     'application/json': jsonify,
-    'text/plain': plaintext}
+    'text/plain': plaintext,
+    'application/xml': xml,
+    'application/xml+atom': atom_xml
+}
 
 
-def payload(context):
+def payload(context) -> str:
+    """Get a payload string based on header Content-Type, defaulting to 'application/json'"""
     headers = context.get('headers', {})
     code = context.get('response_code', 200)
     ct = context.get('content_type',
                      headers.get('Content-Type', 'application/json'))
-    if ct not in formatters:
-        raise NotImplementedError
-    payload = formatters[ct](context.get('payload', ''))
+    try:
+        payload = formatters[ct](context.get('payload', ''))
+    except (KeyError, NotImplementedError):
+        raise NotImplementedError('"Content-Type: {ct}" not implemented'.format(ct=ct))
     return payload
